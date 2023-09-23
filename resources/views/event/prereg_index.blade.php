ba9d4e5040ce9232aadc3ada33761e2f960dac65
unknown <jessie_chien@digeam.com>
2023-09-23 14:54:00 +0800

    v1.4

--- resources/views/event/prereg_index.blade.php
+++ resources/views/event/prereg_index.blade.php
@@ -166,7 +166,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                             <div class='login_user_id' style='text-align:center' data-val='null'></div>
                             <div class="loginbtn"><a href="https://www.digeam.com/login">登入</a></div>
                             <p class="register">※沒有會員嗎?<a href="https://www.digeam.com/register"
-                                    target="blank"><span>▶</span><span>前往註冊</span><span>◀</span></a></p>
+                                    target="blank"><svg class="p2registerarrowL" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 256 512"><path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/></svg><span>前往註冊</span><svg class="p2registerarrowR" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 256 512"><path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z"/></svg></a></p>
                         @endif
 
                         <div class="step2"></div>
