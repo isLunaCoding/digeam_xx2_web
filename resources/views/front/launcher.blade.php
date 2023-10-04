a255727e6d90b164e3c3418f8861184fc39ffaf5
Linnet <linnet_chen@digeam.com>
2023-10-04 09:58:06 +0800

    v1.0.8

--- resources/views/front/launcher.blade.php
+++ resources/views/front/launcher.blade.php
@@ -38,6 +38,17 @@
                             href="http://xx2.ztgame.com/html/news/133c845/000-1503-000c3-284774.shtml" title=""
                             target="_blank"><img
                                 src="https://siteres.ztgame.com/s4/xx2/images/indexdata/2022-01/4.jpg"></a></li>
+                    <li style="display: none;"><a
+                            href="https://xx2.ztgame.com/html/news/134b30d/000-1695-0044d-621801.shtml" title=""
+                            target="_blank"><img
+                                src="https://siteres.ztgame.com/s4/xx2/images/indexdata/2023-09/396-182_1.jpg"></a></li>
+                    <li style="display: none;"><a
+                            href="https://xx2.ztgame.com/html/news/134b0a5/000-1678-0040e-331788.shtml" title=""
+                            target="_blank"><img
+                                src="https://siteres.ztgame.com/s4/xx2/images/indexdata/2023-03/396-182_1.jpg"></a></li>
+                    <li style="display: none;"><a href="http://xx2.ztgame.com/act/gongcefuli/" title=""
+                            target="_blank"><img
+                                src="https://siteres.ztgame.com/s4/xx2/images/indexdata/2022-01/3.jpg"></a></li>
                 </ul>
             </div>
             <div class="hd">
@@ -47,6 +58,9 @@
                     <li class="">3</li>
                     <li class="">4</li>
                     <li class="on">5</li>
+                    <li class="">6</li>
+                    <li class="">7</li>
+                    <li class="">8</li>
                 </ul>
             </div>
             <!--template::ssi_p2p_focus_v3.tpl-->
@@ -55,210 +69,61 @@
         <div class="news-container">
             <!-- 链接 -->
             <div class="head">
-                <a href="javascript:;" class="">全部<i></i></a>
-                <a href="javascript:;">新闻<i></i></a>
-                <a href="javascript:;">公告<i></i></a>
-                <a href="javascript:;">活动<i></i></a>
-                <a href="javascript:;" class="on">版本<i></i></a>
+                <a href="javascript:;">NEW</a>
+                <a href="javascript:;">活動</a>
+                <a href="javascript:;">系統</a>
+                <button class="more" href="javascript:;">More</button>
             </div>
+            <div class="line"></div>
             <!-- 内容 -->
             <div class="context">
                 <ul class="swiper-slide" style="display: none;">
                     <li class="even">
-                        <span class="colum1">【新闻】</span>
+                        <span class="colum1">TOP</span>
                         <span class="colum2"><a
                                 href="http://xx2.ztgame.com//html/news/134b30d/000-1695-0044d-621801.shtml"
-                                target="_blank">9月29日18:00开放新区“国士无双”</a></span>
-                        <span class="colum3">09/25</span>
-                    </li>
-                    <li>
-                        <span class="colum1">【新闻】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/133c845/000-1503-000c3-284774.shtml"
-                                target="_blank">客户端下载与安装报错问题</a></span>
-                        <span class="colum3">08/01</span>
+                                target="_blank">仙俠事前預約開始55555555555555555555555555555555555555</a></span>
+                        <span class="colum3">2023/09/25</span>
                     </li>
                     <li class="even">
-                        <span class="colum1">【活动】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b30d/00c-1695-0044c-612946.shtml"
-                                target="_blank">国庆中秋双节同庆，众活动齐上线！</a></span>
-                        <span class="colum3">09/25</span>
-                    </li>
-                    <li>
-                        <span class="colum1">【公告】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b310/000-1695-00450-882431.shtml"
-                                target="_blank">"金秋国庆锦盒"概率公示 </a></span>
-                        <span class="colum3">09/28</span>
-                    </li>
-                    <li class="even">
-                        <span class="colum1">【版本】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b30f/000-1695-0044f-824921.shtml"
-                                target="_blank">2023年9月28日服务器维护公告 </a></span>
-                        <span class="colum3">09/27</span>
-                    </li>
-
-                </ul>
-                <ul class="swiper-slide" style="display: none;">
-                    <li class="even">
-                        <span class="colum1">【新闻】</span>
+                        <span class="colum1">TOP</span>
                         <span class="colum2"><a
                                 href="http://xx2.ztgame.com//html/news/134b30d/000-1695-0044d-621801.shtml"
-                                target="_blank">9月29日18:00开放新区“国士无双”</a></span>
-                        <span class="colum3">09/25</span>
-                    </li>
-                    <li>
-                        <span class="colum1">【新闻】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/133c845/000-1503-000c3-284774.shtml"
-                                target="_blank">客户端下载与安装报错问题</a></span>
-                        <span class="colum3">08/01</span>
+                                target="_blank">仙俠事前預約開始</a></span>
+                        <span class="colum3">2023/09/25</span>
                     </li>
                     <li class="even">
-                        <span class="colum1">【新闻】</span>
+                        <span class="colum1">TOP</span>
                         <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b300/004-1694-00449-513312.shtml"
-                                target="_blank">"周年感恩锦盒"概率公示 </a></span>
-                        <span class="colum3">09/13</span>
-                    </li>
-                    <li>
-                        <span class="colum1">【新闻】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b2ac/006-1693-00443-206441.shtml"
-                                target="_blank">9月1日18:00开放新区“万古长青”</a></span>
-                        <span class="colum3">08/28</span>
+                                href="http://xx2.ztgame.com//html/news/134b30d/000-1695-0044d-621801.shtml"
+                                target="_blank">仙俠事前預約開始</a></span>
+                        <span class="colum3">2023/09/25</span>
                     </li>
                     <li class="even">
-                        <span class="colum1">【新闻】</span>
+                        <span class="colum1">TOP</span>
                         <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b291/006-1690-0043d-870212.shtml"
-                                target="_blank">8月4日18:00开放新区“余音绕梁”</a></span>
-                        <span class="colum3">08/01</span>
+                                href="http://xx2.ztgame.com//html/news/134b30d/000-1695-0044d-621801.shtml"
+                                target="_blank">仙俠事前預約開始</a></span>
+                        <span class="colum3">2023/09/25</span>
+                    </li>
+                    <li class="even">
+                        <span class="colum1">TOP</span>
+                        <span class="colum2"><a
+                                href="http://xx2.ztgame.com//html/news/134b30d/000-1695-0044d-621801.shtml"
+                                target="_blank">仙俠事前預約開始</a></span>
+                        <span class="colum3">2023/09/25</span>
                     </li>
 
                 </ul>
                 <ul class="swiper-slide" style="display: none;">
-                    <li class="even">
-                        <span class="colum1">【公告】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b310/000-1695-00450-882431.shtml"
-                                target="_blank">"金秋国庆锦盒"概率公示 </a></span>
-                        <span class="colum3">09/28</span>
-                    </li>
-                    <li>
-                        <span class="colum1">【公告】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b2f8/002-1693-00445-790632.shtml"
-                                target="_blank">2023年9月6日服务器互通公告 </a></span>
-                        <span class="colum3">09/04</span>
-                    </li>
-                    <li class="even">
-                        <span class="colum1">【公告】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b29f/000-1692-00441-086041.shtml"
-                                target="_blank"> "七夕挚爱锦盒"概率公示 </a></span>
-                        <span class="colum3">08/16</span>
-                    </li>
-                    <li>
-                        <span class="colum1">【公告】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b245/000-1690-0043b-268343.shtml"
-                                target="_blank">"夏日超跑锦盒"概率公示 </a></span>
-                        <span class="colum3">07/26</span>
-                    </li>
-                    <li class="even">
-                        <span class="colum1">【公告】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b230/00a-1688-00437-456852.shtml"
-                                target="_blank">"夏日炎炎锦盒"概率公示 </a></span>
-                        <span class="colum3">07/05</span>
-                    </li>
-
                 </ul>
                 <ul class="swiper-slide" style="display: none;">
-                    <li class="even">
-                        <span class="colum1">【活动】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b30d/00c-1695-0044c-612946.shtml"
-                                target="_blank">国庆中秋双节同庆，众活动齐上线！</a></span>
-                        <span class="colum3">09/25</span>
-                    </li>
-                    <li>
-                        <span class="colum1">【活动】</span>
-                        <span class="colum2"><a href="https://xx2.ztgame.com/act/gongcefuli/"
-                                target="_blank">资料片新区开启，精彩活动缤纷来袭</a></span>
-                        <span class="colum3">06/09</span>
-                    </li>
-                    <li class="even">
-                        <span class="colum1">【活动】</span>
-                        <span class="colum2"><a href=" https://xx2.ztgame.com/act/gameinfo2023/"
-                                target="_blank">喜迎新资料片，多重活动齐上线</a></span>
-                        <span class="colum3">06/09</span>
-                    </li>
-                    <li>
-                        <span class="colum1">【活动】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b1d1/006-1686-0042e-277470.shtml"
-                                target="_blank">关注官方公众号，参与活动赢丰厚奖励</a></span>
-                        <span class="colum3">06/09</span>
-                    </li>
-                    <li class="even">
-                        <span class="colum1">【活动】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b1d1/000-1686-0042f-277844.shtml"
-                                target="_blank">关注官方贴吧，元宝实物奖励拿不停！</a></span>
-                        <span class="colum3">06/09</span>
-                    </li>
-
-                </ul>
-                <ul class="swiper-slide" style="display: block;">
-                    <li class="even">
-                        <span class="colum1">【版本】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b30f/000-1695-0044f-824921.shtml"
-                                target="_blank">2023年9月28日服务器维护公告 </a></span>
-                        <span class="colum3">09/27</span>
-                    </li>
-                    <li>
-                        <span class="colum1">【版本】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b30e/001-1695-0044e-723086.shtml"
-                                target="_blank">2023年9月27日服务器维护公告 </a></span>
-                        <span class="colum3">09/26</span>
-                    </li>
-                    <li class="even">
-                        <span class="colum1">【版本】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b307/002-1695-0044b-118539.shtml"
-                                target="_blank">2023年9月20日服务器维护公告 </a></span>
-                        <span class="colum3">09/19</span>
-                    </li>
-                    <li>
-                        <span class="colum1">【版本】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b300/000-1694-00448-513219.shtml"
-                                target="_blank">2023年9月13日服务器维护公告 </a></span>
-                        <span class="colum3">09/12</span>
-                    </li>
-                    <li class="even">
-                        <span class="colum1">【版本】</span>
-                        <span class="colum2"><a
-                                href="http://xx2.ztgame.com//html/news/134b2f9/000-1693-00447-900829.shtml"
-                                target="_blank">2023年9月6日服务器维护公告 </a></span>
-                        <span class="colum3">09/05</span>
-                    </li>
-
                 </ul>
 
                 <!--template::ssi_p2p_news_v3.tpl-->
             </div>
         </div>
     </div>
-
-
-
 </body>
 
 </html>
